5cedcc16ee3950baabb4dac89b0caa367ea50118
coledlocks <cole.drozdeks@gmail.com>
2024-10-29 11:18:09 -0400

    Add files via upload

---
+++ project2_module.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+project2_module.py
+Created on Tue Oct 29 10:52:25 2024
+
+@author: coledlocks
+"""
+# importing packages
+import numpy as np
+
+#%% PART 1: load data
+
+# defining a function to read the npz file
+# def load_means(input_file):
+    # load in the npz file we saved in project 1
+
+
+#%% PART 2: resampling data
+
+# def decimate_data(original_signal, decimation_factor):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
