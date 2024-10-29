a3fff92bb2a255614abca121669d8f730d324270
coledlocks <cole.drozdeks@gmail.com>
2024-10-29 12:13:24 -0400

    Update project2_script.py

--- project2_script.py
+++ project2_script.py
@@ -14,4 +14,5 @@ import project1_module as p1m
 #%% PART 1: load data
 
 ecg_voltage, fs, label_samples, label_symbols, subject_id, electrode, units = p1m.load_data('ecg_e0103_half2.npz')
+symbols, trial_time, mean_trail_signal = p2m.load_means('ecg_means.npz')
 
