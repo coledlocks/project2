cc866ffe20d8dffab2ac368289b5a6d1a308004f
coledlocks <cole.drozdeks@gmail.com>
2024-10-29 12:13:43 -0400

    Update project2_module.py

--- project2_module.py
+++ project2_module.py
@@ -12,8 +12,21 @@ import numpy as np
 #%% PART 1: load data
 
 # defining a function to read the npz file
-# def load_means(input_file):
+def load_means(input_file):
     # load in the npz file we saved in project 1
+    # loading dataset
+    data = np.load(input_file)
+    
+    # looping through and printing variable data
+    for variable in data.files:
+        print(variable)
+    
+    # assigning variables
+    symbols = data['symbols']
+    trial_time = data['trial_time']
+    mean_trial_signal = data['mean_trial_signal']
+    
+    return symbols, trial_time, mean_trial_signal # returns symbols, trial_time, mean_trial_signal
 
 
 #%% PART 2: resampling data
