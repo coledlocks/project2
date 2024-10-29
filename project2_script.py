8cb725b00166df25503a3573f73a0f46f81eecee
coledlocks <cole.drozdeks@gmail.com>
2024-10-29 11:18:41 -0400

    Add files via upload

---
+++ project2_script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+project2_script.py
+
+Created on Tue Oct 29 10:57:28 2024
+
+@author: coledlocks
+"""
+# importing packages
+import project2_module as p2m
+import project1_module as p1m
+
+#%% PART 1: load data
+
+ecg_voltage, fs, label_samples, label_symbols, subject_id, electrode, units = p1m.load_data('ecg_e0103_half2.npz')
+
