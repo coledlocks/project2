5691d6e92b2ee0746fcb16fe22964335cf8623e8
matthew-2676 <mbg2676@gmail.com>
2024-11-05 10:54:37 -0500

    Update project2_script.py

--- project2_script.py
+++ project2_script.py
@@ -57,7 +57,7 @@ plt.legend()
 plt.xlim([0.5, 0.51])
 plt.ylim([-0.1,0.1])
 
-# %% 
+# %% PART 3
 normal_index = np.where(symbols == 'N')[0][0]
 
 # Extract the mean signal for the normal beat
@@ -65,6 +65,9 @@ trial_mean = mean_trial_signal[normal_index]
 
 # Normalize the template
 template = p2m.normalize_template(trial_mean)
-print("Units of the template: 1 / V")
 
-# %%
+# %% PART 4
+
+
+
+
