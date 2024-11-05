620fb19e3887fc7e51c2223186d90d2f2578f397
coledlocks <cole.drozdeks@gmail.com>
2024-11-05 11:16:44 -0500

    Update project2_module.py

--- project2_module.py
+++ project2_module.py
@@ -36,23 +36,27 @@ def decimate_data(original_signal, decimation_factor):
     decimated_signal = original_signal[::decimation_factor]
     return decimated_signal
     
-# %% PART 3: normalize signal
+# %% PART 3: normalizing signal
 
 def normalize_template(trial_mean):
+    # calculating the demeaned signal
     demeaned_signal = trial_mean - np.mean(trial_mean)
+    # calculating energy of the demeaned signal
     energy = np.sum(demeaned_signal ** 2)
     template = demeaned_signal / energy
     
     return template
 
-# %% PART 4:
+#%% PART 4: match data to template
 
-
-
-
-
-
-
-
-
-
+def get_template_match(signal_voltage, template):
+    # folding the template
+    folded_template = np.array([])
+    for fold_index in range(len(template)):
+        folded_template = np.append(folded_template, -1*(template[fold_index]))
+    # convolving the folded signal with the voltage to get the correlation
+    template_match = np.convolve(signal_voltage, folded_template, mode='same')
+    return template_match
+    
+    
+    
